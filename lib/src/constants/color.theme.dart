70052d95763d22dd2011b3fd7e34a178356aca25
Ribesh Basnet <90415424+ribesh-code@users.noreply.github.com>
2022-07-11 22:33:27 +0545

    feat: added two new color code.

--- lib/src/constants/color.theme.dart
+++ lib/src/constants/color.theme.dart
@@ -3,4 +3,6 @@ class ColorTheme {
   static const int kprimaryColor = 0xFF061735;
   static const int kSecondaryColor = 0xFF1FA4B9;
   static const int kBackgroundColor = 0xFFF8FCFF;
+  static const int gray = 0xFFE4EBF3;
+  static const int shimmerColor = 0xFFD1DBDE;
 }
